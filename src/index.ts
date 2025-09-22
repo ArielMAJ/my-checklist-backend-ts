4f3c4857dc7906d49947777c7a144223702b4bc8
Ariel Menezes <69123486+ArielMAJ@users.noreply.github.com>
2025-09-22 08:18:53 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import type { Task } from "./types.ts";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 const listaDeTarefas: Task[] = [
   {
